95d5ea7915165ab4e6f85f571bf3dcabdc1e0d9a
John <johnmulvey48@yahoo.com>
2022-07-28 15:39:58 -0400

    commit tree 070306222c0475d83f40308c993c5a830f699511

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
-# Your code goes here!
+class Anagram
+    attr_reader :word
+    def initialize(word)
+        @word = word
+    end
+    def anagram
+        if word = word
+            puts is_anagram
+        end
+end
+
+word1 = Anagram.new(a)
